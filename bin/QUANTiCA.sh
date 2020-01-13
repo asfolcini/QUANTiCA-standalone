7e58c85ef4cb8fe6f0d5d96c9dec814b64fa5fdb
Alberto Sfolcini <a.sfolcini@gmail.com>
2020-01-13 14:43:31 +0100

    Add files via upload

--- bin/QUANTiCA.sh
+++ bin/QUANTiCA.sh
@@ -1,1 +1,1 @@
-java -Dlog4j.configuration=../config/log4j.properties -cp .:../libs/*:../repo/* quantica.Quantica 
+java -Dlog4j.configuration=../config/log4j.properties -cp .:../libs/*:../repo/* quantica.Quantica $1 $2 $3 $4 $5 $6 $7 $8 $9
